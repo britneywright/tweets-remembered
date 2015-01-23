0d97bf4ad1691470d76b405ae0f4432682811ec0
Britney Wright <blw06g@gmail.com>
2015-01-23 16:15:27 -0500

    configure production db

--- app.rb
+++ app.rb
@@ -9,11 +9,26 @@ configure do
   enable :sessions
 end
 
-ActiveRecord::Base.establish_connection(
-  :adapter  => 'postgresql',
-  :database => 'fave_tweets_ar',
-  :encoding => 'unicode'
-)
+configure :development do
+  ActiveRecord::Base.establish_connection(
+    :adapter  => 'postgresql',
+    :database => 'fave_tweets_ar',
+    :encoding => 'unicode'
+  )
+end
+
+configure :production do
+ db = URI.parse(ENV['DATABASE_URL'] || 'postgres:///localhost/mydb')
+
+ ActiveRecord::Base.establish_connection(
+   :adapter  => db.scheme == 'postgres' ? 'postgresql' : db.scheme,
+   :host     => db.host,
+   :username => db.user,
+   :password => db.password,
+   :database => db.path[1..-1],
+   :encoding => 'utf8'
+ )
+end
 
 helpers do
   def current_user?
